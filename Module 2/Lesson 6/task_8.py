1c8265bea973887bc587023bed07fa2caad5dbc3
Ksenia Pestova <liberty.in.smile@yandex.ru>
2024-09-05 21:33:31 +0300

    Корректировка файла

--- Module 2/Lesson 6/task_8.py
+++ Module 2/Lesson 6/task_8.py
@@ -2,5 +2,5 @@ t = input('Введите товары: ').split(', ')
 print('Позиции для перестановки: ')
 a = int(input())
 b = int(input())
-t[a-1], t[b-1]= t[b-1], t[a-1]
+t[a-1], t[b-1] = t[b-1], t[a-1]
 print('На полке: ', (', ').join(t))
